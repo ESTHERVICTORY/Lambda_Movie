0e2a83e57524fe1c1efaa211f255122395f49943
ESTHERVICTORY <46943242+ESTHERVICTORY@users.noreply.github.com>
2019-06-13 21:57:07 -0400

    Add files via upload

---
+++ Instructions/index.js
@@ -0,0 +1,9 @@
+const response = {
+    statusCode: 200,
+    body: <OMDB Data response>
+      headers: {
+        "Access-Control-Allow-Origin":"*",
+        "content-type": "application/json"
+      }
+};
+return response;
